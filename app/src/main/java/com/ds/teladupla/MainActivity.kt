0dd3d6f105a4b260e31737de61d15733d1cb990e
EnzoRafaelPassos <enzorafel14@gmail.com>
2022-06-01 22:28:55 -0300

    TelaDupla

--- app/src/main/java/com/ds/teladupla/MainActivity.kt
+++ app/src/main/java/com/ds/teladupla/MainActivity.kt
@@ -1,11 +1,26 @@
 package com.ds.teladupla
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.AlarmClock.EXTRA_MESSAGE
+import android.view.View
+import android.widget.EditText
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    /** Essa função é chamada sempre que o usuário toca no botão enviar */
+    fun sendMessage(view: View) {
+        val editText = findViewById<EditText>(R.id.editTextTextPersonName)
+        val message = editText.text.toString()
+        val intent = Intent(this, DisplayMessageActivity::class.java).apply {
+            putExtra(EXTRA_MESSAGE, message)
+        }
+        startActivity(intent)
+
     }
 }
